6db4fdf8c5841dd37ca20fa5a3c1224ef0bba256
katrin <slavnova.katya@gmail.com>
2023-02-13 16:32:33 +0300

    Создали программу для бота теххподдержки для приема входящих вопросов

---
+++ support_study_bot.py
@@ -0,0 +1,20 @@
+import telebot
+
+token = "6195946306:AAHMiIO0cOKo7Q1Em7cH42PkMTJhQ5ic4S8"
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["start"])
+def welcome(message):
+    bot.reply_to(message, "Здравствуйте! Я бот техподдержки. Введите команду /question , чтобы оставить свой вопрос")
+@bot.message_handler(commands=["question"])
+def prepare_to_question(message):
+    bot.reply_to(message, "Задайте свой вопрос, а я передам его оператору. Убедитесь, что вопрос написан одним сообщением")
+    bot.register_next_step_handler(message, write_question)
+
+def write_question(message):
+    data = open ("questions.txt", "a", encoding = "utf-8")
+    data.write(f" {message.from_user.id}%%{message.from_user.first_name} {message.from_user.last_name}: {message.text} \n" )
+    data.close()
+    bot.reply_to(message, "Ваш вопрос отправлен оператору. Среднее время ожидания ответа: 1 час. Спасибо за обращение!")
+
+bot.infinity_polling()
